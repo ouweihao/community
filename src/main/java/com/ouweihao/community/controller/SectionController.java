9a864878c620cac631d938dbb41d84e7efc78537
ouweihao <1804400792@qq.com>
2021-05-29 16:04:37 +0800

    分类控制

---
+++ src/main/java/com/ouweihao/community/controller/SectionController.java
@@ -0,0 +1,15 @@
+package com.ouweihao.community.controller;
+
+import com.ouweihao.community.service.SectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SectionController {
+
+    @Autowired
+    private SectionService sectionService;
+
+//    public
+
+}
